82600a5d9aa9554a040fcaa098e6b3f8daa71954
AnzelGo <anzeltechgo@gmail.com>
2025-12-06 11:45:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ async def run_compression_flow(client, chat_id, status_message):
             '-r', '30',  # <-- fps definido aquí
             '-crf', opts['crf'],
             '-preset', opts['preset'],
-            '-vcodec', 'h263',
+            '-vcodec', 'mpeg4',
             '-acodec', 'aac',
             '-b:a', '64k',
             '-movflags', '+faststart',
